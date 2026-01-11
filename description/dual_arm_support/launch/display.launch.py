24108fe256d15b1a55ae03ee515905cc5238dc1e
LHP <li_haopeng_tool@163.com>
2026-01-12 00:39:50 +0800

    export urdf

---
+++ description/dual_arm_support/launch/display.launch.py
@@ -0,0 +1,67 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+
+def generate_launch_description():
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'gui',
+            default_value='true',
+            description='Flag to enable joint_state_publisher_gui'
+        )
+    )
+
+    pkg_share = FindPackageShare('dual_arm_support')
+    urdf_file = PathJoinSubstitution([pkg_share, 'urdf', 'dual_arm_support.urdf'])
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
+            urdf_file,
+        ]
+    )
+    robot_description = {'robot_description': ParameterValue(robot_description_content, value_type=str)}
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        condition=UnlessCondition(LaunchConfiguration('gui')),
+        parameters=[robot_description]
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        condition=IfCondition(LaunchConfiguration('gui')),
+        parameters=[robot_description]
+    )
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', PathJoinSubstitution([pkg_share, 'config', 'urdf.rviz'])],
+    )
+
+    return LaunchDescription(declared_arguments + [
+        joint_state_publisher_node,
+        joint_state_publisher_gui_node,
+        robot_state_publisher_node,
+        rviz_node,
+    ])
